fa5b3fe497c5747c145cc0118ba7480108d26c08
Felipe Costa <felipecostamoreira@gmail.com>
2021-12-09 23:16:01 -0300

    commit de garantia

---
+++ script.js
@@ -0,0 +1,35 @@
+const userCorreto = "admin"
+const senhaCorreta = "123abc"
+
+const start = () => {
+    const loginEvent = document.getElementById('loginButton')
+    loginEvent.onclick = fazerLogin
+}
+
+const fazerLogin = () => {
+
+    const user = document.getElementById('userInput').value
+    const senha = document.getElementById('senhaInput').value
+
+
+
+    if (user === userCorreto && senha === senhaCorreta) {
+        window.open('../logado.html')
+        const p = document.getElementById('pError')
+        p.textContent = "Sucesso!"
+        p.style.color = "green"
+    } 
+    if(user == "" || senha == ""){
+        const p = document.getElementById('pError')
+        p.textContent = "Dados obrigatorios"
+        p.style.color = "red"
+    }
+    else {
+        const p = document.getElementById('pError')
+        p.textContent = "Usuário e/ou senha errado"
+        p.style.color = "red"
+
+    }
+}
+
+start()
